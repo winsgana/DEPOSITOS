4fe19f79dda990cc359d8d2a6235026edcaec599
winsgana <winsganabet@gmail.com>
2025-02-21 18:25:25 -0400

    Update callback.php
    test de botones

--- callback.php
+++ callback.php
@@ -1,3 +1,9 @@
 <?php
-echo "Callback funcionando correctamente";
+$TOKEN = "7957554764:AAHUzfquZDDVEiwOy_u292haqMmPK2uCKDI";
+
+// Capturar los datos enviados por Telegram
+$content = file_get_contents("php://input");
+file_put_contents("log.txt", $content); // Guarda los datos en un archivo
+
+echo "Datos guardados en log.txt";
 ?>
