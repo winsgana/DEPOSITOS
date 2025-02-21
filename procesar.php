3602cc9ea6876a8730ebecf9d71e5639641e4ba3
winsgana <winsganabet@gmail.com>
2025-02-21 11:55:25 -0400

    botones procesar.php
    agregue dos botones para telegran

--- procesar.php
+++ procesar.php
@@ -65,6 +65,13 @@ $caption = "📎 Nuevo QR recibido:\n\n" .
            "💰 Monto: $montoFormatted\n\n" .
            "🔔 Por favor, Realizar el pago.";
 
+$keyboard = json_encode([
+    "inline_keyboard" => [
+        [["text" => "✅ Completado", "callback_data" => "completado"]],
+        [["text" => "❌ Rechazado", "callback_data" => "rechazado"]]
+    ]
+]);
+
 $postData = [
   "chat_id" => $CHAT_ID,
   "document" => new CURLFile($rutaTemporal, mime_content_type($rutaTemporal), $nombreArchivo),
