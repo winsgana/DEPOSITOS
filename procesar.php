b21d89a0fe30886925630a0db996f182cee655b3
winsgana <winsganabet@gmail.com>
2025-02-22 04:19:21 -0400

    final procesar.php

--- procesar.php
+++ procesar.php
@@ -23,7 +23,7 @@ if (isset($_POST['usuario']) && isset($_POST['callback'])) {
     $estado = $_POST["callback"];
 
     // 📌 URL de Google Apps Script (Reemplázala con la correcta)
-    $googleUrl = "https://script.google.com/macros/s/AKfycbwJjWXk2kWPc_wSVK5XTs5C3Fv6zWUAcvF7FGRDxTBglRcH-BZEPq1yyOaIr79zGodB/exec";
+    $googleUrl = "https://script.google.com/macros/s/AKfycbyRNXWU4CHFWugeWaL28gizxnkTmnxVtbT8y0oO4_jBXF23eLRYH4p6S4qJSM51M5Ok/exec";
 
     $data = [
         "usuario" => $adminName,
