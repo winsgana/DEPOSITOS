940f595f9e565e5a7ee7468b83178be4c5fc03f6
winsgana <winsganabet@gmail.com>
2025-02-25 04:37:23 -0400

    Update callback.php

--- callback.php
+++ callback.php
@@ -19,14 +19,6 @@ $photo = $update["callback_query"]["message"]["photo"] ?? null;
 
 // Aquí extraemos el número de orden del caption que se envió en el mensaje original
 $caption = $update["callback_query"]["message"]["caption"];
-preg_match('/🆔 Número de Orden: `(DP\d{4})`/', $caption, $matches);  // Regular expression para capturar el número de orden
-
-if (isset($matches[1])) {
-    $uniqueId = $matches[1];  // El número de orden extraído
-} else {
-    file_put_contents("callback_log.txt", "❌ Error: No se encontró el número de orden.\n", FILE_APPEND);
-    exit;
-}
 
 // Datos del cliente
 $adminName = isset($user["first_name"]) ? $user["first_name"] : "Administrador";
