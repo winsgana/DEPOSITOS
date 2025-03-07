f684d67802540e621f5ea4053098e705481d676d
winsgana <winsganabet@gmail.com>
2025-03-07 04:11:23 -0400

    Create config.php

---
+++ utils/config.php
@@ -0,0 +1,6 @@
+<?php
+// Configuración global
+define('TELEGRAM_TOKEN', '7957554764:AAHUzfquZDDVEiwOy_u292haqMmPK2uCKDI');
+define('TELEGRAM_CHAT_ID', '-4633546693');
+define('SMS_API_KEY', '6d32dd80bef8d29e2652d9c68148193d1ff229c248e8f731');
+?>
