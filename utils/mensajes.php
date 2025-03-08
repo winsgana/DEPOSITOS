129d29ba47a67debe990b3a2ee39c73d658285fa
winsgana <winsganabet@gmail.com>
2025-03-07 22:53:49 -0400

    Update mensajes.php

--- utils/mensajes.php
+++ utils/mensajes.php
@@ -1,10 +1,9 @@
 <?php
 function mensajeRecepcion($fecha, $monto) {
-    return "📢 Confirmación de Solicitud de Depósito\n\n" .
+    return "✅ Su solicitud ha sido recibida\n\n" .
            "🗓 Fecha: $fecha\n" .
            "💰 Monto: $monto BOB\n\n" .
-           "Tu solicitud ha sido recibida con éxito y está en proceso. Te informaremos una vez que haya sido completada."
-           "🔔 Recuerda que este canal es exclusivamente para notificaciones automáticas. Si necesitas asistencia, por favor contacta a nuestro equipo de soporte por los medios oficiales.";
+           "🔔 Te notificaremos cuando esté procesada.";
 }
 
 function mensajeCompletado() {
@@ -15,11 +14,8 @@ function mensajeCompletado() {
 }
 
 function mensajeRechazado() {
-    return "⚠️ Tu solicitud de depósito ha sido rechazada.\n\n" .
-           "Esto puede deberse a que no cumples con nuestras políticas de verificación de identidad (KYC) o porque los datos ingresados son incorrectos.\n\n" .
-           "Para conocer más detalles y resolver esta situación, te pedimos que contactes a nuestro equipo de soporte:\n" .
-           "📱 WhatsApp: [http://wa.me/59162162190]\n" .
-           "📧 Correo: [soporte@winsgana.com]\n" .
+    return "⚠️ Tu solicitud fue rechazada.\n\n" .
+           "Si crees que esto fue un error, puedes comunicarte con nuestro equipo de soporte para ayudarte.\n\n" .
            "🔔 Recuerda: este es un canal de notificaciones automáticas.";
 }
 ?>
